5d05752a958353a2827fba784e15a9c95d85ebae
shauryabahuguna-tmpl <shaurya.b@tunica.tech>
2024-04-12 18:09:53 +0530

    Chanes to api

--- commonFunctions.js
+++ commonFunctions.js
@@ -1,11 +1,15 @@
 const sortedData = require('./sortedDeliData.json');
 const tunicaData = require('./tunicaData.json');
+const jhData = require('./JamesHardieProductData.json')
 
 module.exports = {
   anythingLLMApi: async (Props) => {
     let workspace = Props.workspace;
     let token = Props.token;
     let userQuery = Props.userQuery;
+    let client = Props.client;
+
+    let queryAddition = `- Provide response in format specified in the object: /n { “message”: “Response to the above question, only the response of at least 100 words”, “followup”: [<An array of at least three follow-up questions related to the current question that the user might ask, return only questions> ] } REMEMBER: Only provide response in the object format provided above. Answer as if you are ` + client + ` itself.`
 
     var myHeaders = {
       "Authorization": "Bearer " + token,
@@ -13,8 +17,8 @@ module.exports = {
     };
 
     var raw = JSON.stringify({
-      message: userQuery,
-      mode: "chat",
+      message: userQuery + queryAddition,
+      mode: "query",
     });
 
     var requestOptions = {
@@ -25,10 +29,12 @@ module.exports = {
     };
 
     const responce = await fetch(
-      "http://localhost:3001/api/v1/workspace/" + workspace + "/chat",
+      "http://192.168.1.48:3001/api/v1/workspace/" + workspace + "/chat",
       requestOptions
     );
     data = await responce.json();
+
+    console.log(data, "------data------")
 
     return data.textResponse;
   },
@@ -40,7 +46,7 @@ module.exports = {
     let userQuery = Props.userQuery;
     let summarizationText = Props.summarizationText;
 
-    let LLMquery = 'Based on the user query below and the summarization text provided below give an array of at most 4 URLs that corresponds to them in the format: ["url 1", "url 2", "url 3"] only return the URLs and only if they exist. Remember the format of the response. Only return array and nothing else.' + '\n user Query: ' + userQuery + '\n summarizationText: ' + summarizationText
+    let LLMquery = 'user Query: ' + userQuery + '\n summarizationText: ' + summarizationText + '\n Based on the user query and the summarization text provided above give an array of at most 4 URLs that corresponds to them in the format: [“url 1”, “url 2”, “url 3”]. I only want an array not explanation'
 
     console.log(LLMquery)
 
@@ -62,7 +68,7 @@ module.exports = {
     };
 
     const responce = await fetch(
-      "http://localhost:3001/api/v1/workspace/" + workspace + "/chat",
+      "http://192.168.1.48:3001/api/v1/workspace/" + workspace + "/chat",
       requestOptions
     );
     data = await responce.json();
@@ -99,12 +105,12 @@ module.exports = {
           "productImg": "https://res.cloudinary.com/tmplwebsite/image/upload/v1636696533/James_Hardie_Banner_26eca2378f.webp"
         },
         {
-            "title": "TimesPro  Tunica Tech",
-            "url": "https://www.tunica.tech/projects/times-pro",
-            "image": "",
-            "product": "TimesPro",
-            "productDescription": "",
-            "productImg": "https://res.cloudinary.com/tmplwebsite/image/upload/v1699679636/Bannertimes_487e91fc67.webp"
+          "title": "TimesPro  Tunica Tech",
+          "url": "https://www.tunica.tech/projects/times-pro",
+          "image": "",
+          "product": "TimesPro",
+          "productDescription": "",
+          "productImg": "https://res.cloudinary.com/tmplwebsite/image/upload/v1699679636/Bannertimes_487e91fc67.webp"
         },
         {
           "title": "Vodafone Oman  Together we can  Tunica Tech",
@@ -122,7 +128,7 @@ module.exports = {
           "productDescription": "",
           "productImg": "https://res.cloudinary.com/tmplwebsite/image/upload/v1636692800/Purl_Banner_5071240170.webp"
         },
-      ]
+        ]
         break;
       case "Sorted":
         // Change jsonData to sortedData or any other source specific to Sorted client
@@ -143,6 +149,26 @@ module.exports = {
           "productDescription": "Sorted Buckwheat Pizza Crust | The best Gluten Free Pizza Crust for Starters | Tastes the same at Whole Wheat Pizza with No Gluten and definitely no guilt",
           "productImg": "https://www.sorteddeli.com/wp-content/uploads/2021/09/buckwheat-crust-445x445.jpg"
         }]
+        break;
+      case "JamesHardie":
+        // Change jsonData to sortedData or any other source specific to Sorted client
+        clientSpecificData = jhData;
+        defaultData = [
+          {
+            "title": "James Hardie™ Architectural Collection: Elevate Your Design",
+            "url": "https://www.jameshardie.com.au/hardie-architectural-collection",
+            "product": "Hardie™ Architectural Collection",
+            "productDescription": "",
+            "productImg": "https://images.ctfassets.net/rg5y8r6t6cjr/1gfourTo5bVVRWDER4nRGE/3ac36a2dc7bef7d6de8861aa23669be1/brushed-concrete-axon-boxmodern-__6_.webp"
+          },
+          {
+            "title": "Hardie Oblique Cladding for Modern Farm House Design | James Hardie",
+            "url": "https://www.jameshardie.com.au/productrange/hardie-oblique-cladding",
+            "product": "Hardie™ Oblique™ Cladding",
+            "productDescription": "A fresh slant on modern home design",
+            "productImg": "https://images.ctfassets.net/rg5y8r6t6cjr/rAVZ0Dwf0feCiLkDY5b6I/4db6e91aa274930772ec00efec1b8b3a/oblique-cladding-barn-jameshardie-4.jpg"
+          }
+        ]
         break;
       default:
         break;
